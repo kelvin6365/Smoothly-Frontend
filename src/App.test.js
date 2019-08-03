ec35cfce9e14a9e792a08e8927ab56f3224820d3
kelvin6365 <kelvin6365@gmail.com>
2019-08-04 02:26:03 +0800

    test setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const wrapper = shallow(<App />);
+describe('<App unit test case />', () => {
+	it('renders without crashing', () => {
+		expect(wrapper).toHaveLength(1);
+	});
 });
