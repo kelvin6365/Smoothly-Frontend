ae4cd868e2ef1fc01d3ad635d79b7014a02e56a0
kelvin6365 <kelvin6365@gmail.com>
2019-09-03 15:32:07 +0800

    setup dev backend url

--- src/Config/system.js
+++ src/Config/system.js
@@ -4,6 +4,8 @@ const autoSwitch = () => {
 		return 'http://localhost:3099/api';
 	} else if (window.location.hostname == '10.96.20.74') {
 		return 'http://10.96.20.74:3010/api';
+	} else if (window.location.hostname == 'smoothly.dev.dreamplugs.com') {
+		return 'http://smoothly.dev.dreamplugs.com/Smoothly-Backend/public/graphql';
 	}
 };
 
