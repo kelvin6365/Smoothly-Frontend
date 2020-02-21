4037f40590b7672820e13d90433bf08b19cc9a6b
Kelvin <kelvin6365@gmail.com>
2020-02-21 19:47:30 +0800

    system api change

--- src/Config/system.js
+++ src/Config/system.js
@@ -1,14 +1,14 @@
 const autoSwitch = () => {
-	//TBC  this change only for Marcus to have a demo
-	if (window.location.hostname == 'localhost') {
-		return 'http://localhost:3099/api';
-	} else if (window.location.hostname == '10.96.20.74') {
-		return 'http://10.96.20.74:3010/api';
-	} else if (window.location.hostname == 'smoothly.dev.dreamplugs.com') {
-		return 'https://smoothly.dev.dreamplugs.com/Smoothly-Backend/public/graphql';
-	}
+  //TBC  this change only for Marcus to have a demo
+  if (window.location.hostname == "localhost") {
+    return "http://localhost:3099/api";
+  } else if (window.location.hostname == "192.168.1.134") {
+    return "http://192.168.1.134:3099/api";
+  } else if (window.location.hostname == "smoothly.dev.dreamplugs.com") {
+    return "https://smoothly.dev.dreamplugs.com/Smoothly-Backend/public/graphql";
+  }
 };
 
 export const config = {
-	graphqQL_url: autoSwitch()
+  graphqQL_url: autoSwitch()
 };
